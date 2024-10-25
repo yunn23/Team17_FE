8d2f584e19d919fc4d01bfccf3e29714a225ef74
JeongSeoyun <xeonj@naver.com>
2024-10-25 16:42:25 +0900

    feat: 로그인 기능 react query 도입

--- src/pages/KakaoRedirect.tsx
+++ src/pages/KakaoRedirect.tsx
@@ -1,38 +1,39 @@
-import { useEffect } from 'react'
 import { useNavigate } from 'react-router'
+import { useQuery } from '@tanstack/react-query'
+import { useEffect } from 'react'
 import axiosInstance from '../api/axiosInstance'
+
+const fetchLogin = async (code: string | null): Promise<string> => {
+  const { data } = await axiosInstance.get(`/api/oauth/login`, {
+    params: {
+      code,
+    }
+  })
+  return data
+}
 
 const KakaoRedirect = () => {
   const navigate = useNavigate()
+  const code = new URL(document.location.toString()).searchParams.get('code')
+
+  const { data: tokenData, isSuccess, isLoading, isError, error: queryError } = useQuery({
+    queryKey: ['kakaoLogin', code],
+    queryFn: () => fetchLogin(code),
+    enabled: !!code,
+    retry: 1
+  })
 
   useEffect(() => {
-    const code = new URL(document.location.toString()).searchParams.get('code')
-
-    const fetchLogin = async () => {
-      try {
-        const response = await axiosInstance.get(`/api/oauth/login`, {
-          params: {
-            code,
-          },
-        })
-
-        const token = response.data
-
-        if (token) {
-          localStorage.setItem('authToken', token)
-          navigate('/')
-        }
-      } catch (error) {
-        // 추후 에러 처리 예정
-        // eslint-disable-next-line no-console
-        console.error('로그인 에러: ', error)
-      }
+    if (isSuccess) {
+      localStorage.setItem('authToken', tokenData)
+      navigate('/')
     }
+  })
 
-    fetchLogin()
-  }, [navigate])
+  if (isLoading) return <div>로그인 중 ...</div>
+  if (isError) return <div>에러가 발생했습니다: {`${(queryError as Error).message}`}</div>
 
-  return <div>KakaoRedirect</div>
+  return <div>로그인 리다이렉트 중</div>
 }
 
 export default KakaoRedirect
