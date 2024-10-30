2cc71b3ef2f629d26ec7446fba747500464fb819
JeongSeoyun <xeonj@naver.com>
2024-10-30 14:02:48 +0900

    feat: 토큰 만료시 로그인 페이지로 리다이렉트

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,22 @@
 import axios from 'axios'
+import { useNavigate } from 'react-router'
 
 const axiosInstance = axios.create({
   baseURL: 'https://home-try.13.125.102.156.sslip.io',
   timeout: 20000,
   headers: { 'Content-Type': 'application/json' },
 })
+
+export const setupAxiosInterceptor = (navigate: ReturnType<typeof useNavigate>) => {
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401) {
+        navigate('/login')
+      }
+      return Promise.reject(error)
+    }
+  )
+}
 
 export default axiosInstance
