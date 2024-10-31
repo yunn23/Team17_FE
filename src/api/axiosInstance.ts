fa847f569bf2d23fb4fa1cd054bafef455eccc1e
JeongSeoyun <xeonj@naver.com>
2024-10-31 22:16:41 +0900

    feat: 인가코드 및 토큰 관련 에러 처리

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -16,6 +16,12 @@ export const setupAxiosInterceptor = (
       if (error.response?.status === 401) {
         navigate('/login')
       }
+      if (error.response?.data?.errorCode === 'Member400_001') {
+        navigate('/login')
+      }
+      if (error.response?.data?.errorCode === 'Auth400_001') {
+        navigate('/login')
+      }
       return Promise.reject(error)
     }
   )
